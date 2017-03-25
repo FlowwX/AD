7b4b3aacc78a09748e8a7909cd1d9b5e4d3ac54a
AlexLoeffel <muskevier@aol.com>
2017-03-25 14:33:27 +0100

    first implementation of list as array

---
+++ AlgorithmenDatenstrukturen/src/Aufgabenblatt01/A/KnotenA.java
@@ -0,0 +1,15 @@
+package Aufgabenblatt01.A;
+
+import Aufgabenblatt01.Knoten;
+
+/**
+ * @author Alexander Mendel
+ *
+ */
+public class KnotenA extends Knoten<Integer> {
+
+	public KnotenA(Integer daten) {
+		super(daten);
+	}
+	
+}
