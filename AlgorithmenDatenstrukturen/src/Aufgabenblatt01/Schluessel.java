6d5933b410437823696f171cea751b88d04fa30d
Alf_Ir0n <florian.heuer89@gmail.com>
2017-03-26 18:42:23 +0200

    Konstruktor für Schlüssel hinzugefügt

--- AlgorithmenDatenstrukturen/src/Aufgabenblatt01/Schluessel.java
+++ AlgorithmenDatenstrukturen/src/Aufgabenblatt01/Schluessel.java
@@ -5,6 +5,10 @@ public class Schluessel {
 	private static int nummer = 1000;
 	private int wert;
 	
+	public Schluessel(int wert) {
+		wert = wert;
+	}
+	
 	public Schluessel() {
 		nummer++;
 		wert = nummer;
