d16fda8ad2b9f309b09e241afbc1db195e873b86
Flo <Flo@MAC>
2017-03-29 23:44:21 +0200

    Testlauf drin

--- AlgorithmenDatenstrukturen/src/Aufgabenblatt02/PrimzahlSuche.java
+++ AlgorithmenDatenstrukturen/src/Aufgabenblatt02/PrimzahlSuche.java
@@ -14,27 +14,56 @@ public class PrimzahlSuche {
 	/**
 	 * 
 	 */
-	public static int K = 2;
+	public static long zaehlerLangsam = 0;
 	
 	/**
 	 * 
 	 */
-	public static final int N = (int) Math.pow(10, K);
+	public static long zaehlerSchnell = 0;
 	
 	/**
 	 * 
 	 */
-	public static boolean[] langsam(){
+	public static long zaehlerSieb = 0;
+	
+	/**
+	 * 
+	 */
+	public static long zaehlerEinzeln = 0;
+	
+	/**
+	 * 
+	 */
+	public static final int KMAX = 5;
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 */
+	public static boolean[] langsam(int n){
 
-		boolean[] feld = new boolean[N];
+		boolean[] feld = new boolean[n];
 		
 		//fülle array
-		for(int i=0 ; i < N ; i++){
+		for(int i=0 ; i < n ; i++){
 			feld[i] = true;
 		}
 		
-		for ( int i = 2; i < N; i++){
-			for ( int j = 2; j < N; j++){
+		for ( int i = 2; i < n; i++){
+			
+			//AUFWAND INKREMENTIEREN
+			zaehlerLangsam++;
+			
+			for ( int j = 2; j < n; j++){
+				
+				//AUFWAND INKREMENTIEREN
+				zaehlerLangsam++;
+				
 				if ( (i%j == 0) && (j!=i) ){
 					feld[i] = false;
 				}
@@ -47,17 +76,25 @@ public class PrimzahlSuche {
 	/**
 	 * 
 	 */
-	public static boolean[] schnell(){
+	public static boolean[] schnell( int n){
 		
-		boolean[] feld = new boolean[N];
+		boolean[] feld = new boolean[n];
 		
 		//fülle array
-		for(int i=0 ; i < N ; i++){
+		for(int i=0 ; i < n ; i++){
 			feld[i] = true;
 		}
 		
-		for ( int i = 2; i < Math.sqrt(N); i++){
+		for ( int i = 2; i < Math.sqrt(n); i++){
+			
+			//AUFWAND INKREMENTIEREN
+			zaehlerSchnell++;
+			
 			for ( int j = 2; j < i; j++){
+				
+				//AUFWAND INKREMENTIEREN
+				zaehlerSchnell++;
+				
 				if ( i%j == 0 ){
 					feld[i] = false;
 				}
@@ -70,19 +107,26 @@ public class PrimzahlSuche {
 	/**
 	 * 
 	 */
-	public static boolean[] sieb(){
+	public static boolean[] sieb( int n ){
 		
-		boolean[] feld = new boolean[N];
+		boolean[] feld = new boolean[n];
 		
 		//fülle array
-		for(int i=0 ; i < N ; i++){
+		for(int i=0 ; i < n ; i++){
 			feld[i] = true;
 		}
 			
-		for(int i=2 ; i < Math.sqrt(N) ; i++){
-	
+		for(int i=2 ; i < (int)Math.sqrt(n) ; i++){
+			
+			//AUFWAND INKREMENTIEREN
+			zaehlerSieb++;
+				
 				if ( feld[i] == true ){
-					for (int j = 2 ; i*j < N ; j++){
+					for (int j = 2 ; i*j < n ; j++){
+						
+						//AUFWAND INKREMENTIEREN
+						zaehlerSieb++;
+						
 						feld[i*j] = false;
 					}
 				}
@@ -96,6 +140,10 @@ public class PrimzahlSuche {
 	 */
 	public static boolean einzeln(int zahl){
 		for (int j = 2 ; j < zahl ; j++){
+			
+			//AUFWAND INKREMENTIEREN
+			zaehlerEinzeln++;
+			
 			if(zahl%j==0){
 				return false;
 			}
@@ -104,15 +152,37 @@ public class PrimzahlSuche {
 	}
 	
 	/**
+	 * Hauptprogramm
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		boolean[] feld = PrimzahlSuche.sieb();
 		
-		System.out.println(feld[4]);
 		
-		System.out.println(PrimzahlSuche.einzeln(7));
+		for(int k=1; k<=KMAX; k++){
+			int n = (int) Math.pow(10, k);
+			
+			System.out.println("----K="+k+"----");
+			
+			PrimzahlSuche.langsam( n );
+			System.out.println("Langsame Suche: " + PrimzahlSuche.zaehlerLangsam);
+			
+			PrimzahlSuche.schnell( n );
+			System.out.println("Schnelle Suche: " + PrimzahlSuche.zaehlerSchnell);
+			
+			PrimzahlSuche.sieb( n );
+			System.out.println("Sieb Suche: " + PrimzahlSuche.zaehlerSieb);
+			
+			for(int i=2; i<n; i++){
+				PrimzahlSuche.einzeln( n );
+			}
+			System.out.println("Einzelne Suche: " + PrimzahlSuche.zaehlerEinzeln + "\n");
+		}
+		
+
+
+		
 	}
 
 }
