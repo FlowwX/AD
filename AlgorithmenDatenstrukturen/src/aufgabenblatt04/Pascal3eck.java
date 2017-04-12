943c8a7136f24a9c61edcf59df037b7f3d49fc04
Alf_Ir0n <florian.heuer89@gmail.com>
2017-04-12 17:24:46 +0200

    Interface reingewämmst

---
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt04/Pascal3eck.java
@@ -0,0 +1,11 @@
+package aufgabenblatt04;
+
+public interface Pascal3eck {
+
+	/**
+	 * Liefert die n-te Zeile des Pascal'schen Dreiecks zurück.
+	 * @return n-te Zeile als Integer Array 
+	 */
+	public int[] getZeile( int n );
+	
+}
