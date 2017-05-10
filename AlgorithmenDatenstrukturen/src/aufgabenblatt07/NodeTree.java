0d5a095a1eab31e455f9b89569e252cb906537a1
Alf_Ir0n <florian.heuer89@gmail.com>
2017-05-10 18:08:18 +0200

    Aufgabe 7 Interface/Abtract Class hinzugefügt

---
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt07/NodeTree.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package aufgabenblatt07;
+
+/**
+ * @author 
+ *
+ */
+public class NodeTree<T> extends Tree<T> {
+
+	@Override
+	public void insert(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T getLeftChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getRightChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
---
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt07/Tree.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package aufgabenblatt07;
+
+/**
+ * @author 
+ *
+ */
+public abstract class Tree<T> {
+	
+	/**
+	 * 
+	 */
+	public abstract void insert( T item );
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public abstract T getLeftChild();
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public abstract T getRightChild();
+	
+	/**
+	 * 
+	 */
+	public void preOrder() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void postOrder() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void inOrder() {
+		
+	}
+	
+}
