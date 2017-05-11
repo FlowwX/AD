a0336f14b328af0fb04a4b03c0b8c8d79ab90255
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-05-11 13:10:41 +0200

    ArrayTree helpers

--- AlgorithmenDatenstrukturen/src/aufgabenblatt07/ArrayTree.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt07/ArrayTree.java
@@ -2,29 +2,47 @@ package aufgabenblatt07;
 
 import java.util.ArrayList;
 
-public class ArrayTree<T extends Comparable<T>> extends Tree<T>{
+public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 	private ArrayList<T> arr;
-	
-	public ArrayTree(int size){
-		arr = new ArrayList<T>(size);
+
+	public ArrayTree(int size) {
+		arr = new ArrayList<T>(size + 1);
 	}
 
 	@Override
 	public boolean insert(T item) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public T getLeftChild(T parent) {
-		// TODO Auto-generated method stub
-		return null;
+		int ind = getLeftChildInd(getIndexOf(parent));
+		return ind > 0 ? arr.get(ind) : null;
 	}
 
 	@Override
 	public T getRightChild(T parent) {
-		// TODO Auto-generated method stub
-		return null;
+		int ind = getRightChildInd(getIndexOf(parent));
+		return ind > 0 ? arr.get(ind) : null;
 	}
 
+	private int getLeftChildInd(int parentInd) {
+		return parentInd > 0 ? 2 * parentInd : 0;
+	}
+
+	private int getRightChildInd(int parentInd) {
+		return parentInd > 0 ? 2 * parentInd + 1 : 0;
+	}
+
+	private int getIndexOf(T item) {
+		int i = 1;
+		int cmp = item.compareTo(arr.get(i));
+		while (cmp != 0 && i <= arr.size()) {
+			if (cmp < 0)
+				i = getLeftChildInd(i);
+			else
+				i = getRightChildInd(i);
+		}
+		return i <= arr.size() ? i : 0;
+	}
 }
