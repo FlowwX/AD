1b8d536841cb7ffea703ae9b08ce4794508da809
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-05-11 12:45:21 +0200

    started java comments

--- AlgorithmenDatenstrukturen/src/aufgabenblatt07/Tree.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt07/Tree.java
@@ -3,48 +3,75 @@
  */
 package aufgabenblatt07;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
- * @author 
+ * @author
  *
  */
 public abstract class Tree<T extends Comparable<T>> {
-	
+
 	/**
+	 * @brief Inserts item T into the tree at the right position according to is
+	 *        value
+	 * @param item:
+	 *            Item to insert into the tree. Must not be null. There must not
+	 *            already be another item with the same value (regarding its
+	 *            comparisons) in the tree.
+	 * @return True if item was inserted, false if item was already in the tree
+	 * @exception InvalidArgumentException
+	 *                if item is null
 	 * 
 	 */
-	public abstract void insert( T item );
-	
+	public abstract boolean insert(T item);
+
 	/**
-	 * 
-	 * @return
+	 * @brief Returns the left (smaller) child of a parent node
+	 * @param parent:
+	 *            The parent node to get the child of
+	 * @return Child item, if the parent has a left child. null, if the parent
+	 *         is not in the tree. null, if the parent has no left child.
+	 * @exception InvalidArgumentException
+	 *                if parent is null
 	 */
-	public abstract T getLeftChild();
-	
+	public abstract T getLeftChild(T parent);
+
 	/**
-	 * 
-	 * @return
+	 * @brief Returns the right (greater) child of a parent node
+	 * @param parent:
+	 *            The parent node to get the child of
+	 * @return Child item, if the parent has a right child. null, if the parent
+	 *         is not in the tree. null, if the parent has no right child.
+	 * @exception InvalidArgumentException
+	 *                if parent is null
 	 */
-	public abstract T getRightChild();
-	
+	public abstract T getRightChild(T parent);
+
 	/**
-	 * 
+	 * @brief Gets the items in tree in preOrder ordering
+	 * @return Array of T with all the items currently in the tree
 	 */
-	public void preOrder() {
-		
+	public T[] preOrder() {
+		throw new NotImplementedException();
+		//return null;
 	}
-	
+
 	/**
-	 * 
+	 * @brief Gets the items in tree in postOrder ordering
+	 * @return Array of T with all the items currently in the tree
 	 */
 	public void postOrder() {
-		
+		throw new NotImplementedException();
+		//return null;
 	}
-	
+
 	/**
-	 * 
+	 * @brief Gets the items in tree in inOrder ordering
+	 * @return Array of T with all the items currently in the tree
 	 */
 	public void inOrder() {
-		
+		throw new NotImplementedException();
+		//return null;
 	}
-	
+
 }
