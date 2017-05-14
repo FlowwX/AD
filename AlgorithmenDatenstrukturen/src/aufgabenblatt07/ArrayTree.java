2863f58cc9d85b4f4b466549e189547149955e09
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-05-14 10:17:31 +0200

    Formatting and comments

--- AlgorithmenDatenstrukturen/src/aufgabenblatt07/ArrayTree.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt07/ArrayTree.java
@@ -1,49 +1,77 @@
 package aufgabenblatt07;
 
 /**
- * TODO Error checking
- * TODO Inser(T,int) noetig?
- * @author lasse
+ * TODO Error checking TODO Inser(T,int) noetig?
  *
  * @param <T>
  */
 public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
-	private class customArray<AT>{
+	/**
+	 * Helper class: Generic array with automatically increasing size.
+	 */
+	private class customArray<AT> {
 		private AT[] arr;
 
-		public customArray(){
+		public customArray() {
 			this(20);
 		}
 
 		@SuppressWarnings("unchecked")
-		public customArray(int size){
-			arr = (AT[])new Object[size];
+		public customArray(int size) {
+			arr = (AT[]) new Object[size];
 		}
 
-		public AT get(int index){
-			if(index >= 0 && index < arr.length){
+		/**
+		 * @brief Gets an item from the array.
+		 * @param index
+		 *            Should be positive, will return null for all indices < 0
+		 * @return Element at index, if present. null elsewise.
+		 */
+		public AT get(int index) {
+			if (index >= 0 && index < arr.length) {
 				return arr[index];
-			} else return null;
+			} else
+				return null;
 		}
 
+		/**
+		 * @brief Sets an element of the array to given item. The element is
+		 *        specified by the index. If the index is < 0, the array remains
+		 *        unchanged. If the index is outside the array range, the array
+		 *        is extended to the required size. Newly generated elements are
+		 *        set to null. Any pre-existing item at the index will be
+		 *        overriden.
+		 * @param index
+		 *            Position in the array to set. Must be >= 0.
+		 * @param item
+		 *            Item to set at the index.
+		 */
 		@SuppressWarnings("unchecked")
-		public void set(int index, AT item){
-			if(index < 0) return;
-			if(index >= arr.length){
+		public void set(int index, AT item) {
+			if (index < 0)
+				return;
+			if (index >= arr.length) {
 				Object[] tmpArr = new Object[index + 1];
 				System.arraycopy(arr, 0, tmpArr, 0, arr.length);
-				arr = (AT[])tmpArr;
+				arr = (AT[]) tmpArr;
 			}
 			arr[index] = item;
 		}
 
-		public int size(){
+		/**
+		 * Returns the physical length of the array, regardless of how many
+		 * actual items are stored in it.
+		 *
+		 * @return Size of the array
+		 */
+		public int size() {
 			return arr.length;
 		}
 	}
+
 	private customArray<T> arr;
 
-	public ArrayTree(){
+	public ArrayTree() {
 		arr = new customArray<T>();
 		arr.set(0, null);
 	}
@@ -84,7 +112,7 @@ public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 	}
 
 	@Override
-	protected T getRoot(){
+	protected T getRoot() {
 		return arr.get(1);
 	}
 
@@ -96,7 +124,7 @@ public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 		while (arr.get(indParent) != null) {
 			indLeft = getLeftChildInd(indParent);
 			indRight = getRightChildInd(indParent);
-				
+
 			int cmpParent = item.compareTo(arr.get(indParent));
 			if (cmpParent < 0) {
 				if (arr.get(indLeft) == null) {
@@ -110,7 +138,8 @@ public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 				} else {
 					indParent = indRight;
 				}
-			} else return 0;
+			} else
+				return 0;
 		}
 		return indParent;
 	}
@@ -138,11 +167,11 @@ public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 		int i = 1;
 
 		T node = arr.get(i);
-		while(node != null){
+		while (node != null) {
 			int cmp = item.compareTo(node);
-			if(cmp == 0)
+			if (cmp == 0)
 				return i;
-			else if(cmp < 0)
+			else if (cmp < 0)
 				i = getLeftChildInd(i);
 			else
 				i = getRightChildInd(i);
@@ -156,7 +185,7 @@ public class ArrayTree<T extends Comparable<T>> extends Tree<T> {
 		String str = "[ ";
 		for (int i = 0; i < arr.size(); i++) {
 			T tmp = arr.get(i);
-			str +=  tmp == null ? "null " : tmp.toString() + " ";
+			str += tmp == null ? "null " : tmp.toString() + " ";
 		}
 		return str + "]";
 	}
