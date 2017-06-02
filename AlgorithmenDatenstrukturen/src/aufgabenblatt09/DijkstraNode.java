656c0f146a2fa325099c6b947ec95808bb032814
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-06-02 20:50:42 +0200

    HashCode and Equals

--- AlgorithmenDatenstrukturen/src/aufgabenblatt09/DijkstraNode.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/DijkstraNode.java
@@ -1,7 +1,28 @@
 package aufgabenblatt09;
 
 public class DijkstraNode<T> {
-	public T payload;
+	public Node<T> node;
 	public DijkstraNode<T> next;
 	public int cost;
+
+	public DijkstraNode(Node<T> node, DijkstraNode<T> next, int cost) {
+		if(next == null) next = this;
+		this.node = node;
+		this.next = next;
+		this.cost = cost;
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(this == o) return true;
+		if(o == null) return false;
+		if(!(o instanceof DijkstraNode<?>)) return false;
+		DijkstraNode<T> on = (DijkstraNode<T>) o;
+		return this.node.equals(on.node);
+	}
+
+	@Override
+	public int hashCode(){
+		return this.node.uid;
+	}
 }
--- AlgorithmenDatenstrukturen/src/aufgabenblatt09/Node.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/Node.java
@@ -6,15 +6,29 @@ package aufgabenblatt09;
 public class Node<T> {
 	public T payload;
 	public final int uid;
-	
+
 	private static int nextUid = 0;
-	
+
 	public Node(){
 		this(null);
 	}
-	
+
 	public Node(T payload){
 		this.payload = payload;
 		this.uid = nextUid++;
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(this == o) return true;
+		if(o == null) return false;
+		if(!(o instanceof Node<?>)) return false;
+		Node<T> on = (Node<T>) o;
+		return this.uid == on.uid;
+	}
+
+	@Override
+	public int hashCode(){
+		return uid;
 	}
 }
