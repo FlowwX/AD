5e0f8f7f33aef38ce9d51c907bdd9d6f3e3db972
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-06-02 20:49:49 +0200

    add builder for fixed test graph

--- AlgorithmenDatenstrukturen/src/aufgabenblatt09/GraphWeightedAdjacencyFactory.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/GraphWeightedAdjacencyFactory.java
@@ -23,4 +23,30 @@ public class GraphWeightedAdjacencyFactory {
 		
 		return wa;
 	}
+	static public Node<?> dest;
+	@SuppressWarnings({ "unchecked" })
+	static <T> WeightedAdjacency getSpecialEmptyNodesAdjacency(int nodeCount, AdjacencyImplementation impl){
+		WeightedAdjacency wa;
+		if(impl == AdjacencyImplementation.MATRIX)
+			wa = new WeightedAdjacencyMatrix(nodeCount);
+		else
+			wa = new WeightedAdjacencyList();
+		
+		Node<T>[] nodes = (Node<T>[]) new Node[nodeCount];
+		for(int i = 0; i < 6; i++){
+			nodes[i] = new Node<T>(null);
+		}
+		
+		dest = nodes[5];
+		
+		wa.insert(new WeightedEdge<T>(nodes[0], nodes[1], 1)); // A->B
+		wa.insert(new WeightedEdge<T>(nodes[0], nodes[2], 6)); // A->C
+		wa.insert(new WeightedEdge<T>(nodes[0], nodes[3], 7)); // A->D
+		wa.insert(new WeightedEdge<T>(nodes[1], nodes[3], 1)); // B->D
+		wa.insert(new WeightedEdge<T>(nodes[1], nodes[4], 5)); // B->E
+		wa.insert(new WeightedEdge<T>(nodes[2], nodes[5], 3)); // C->F
+		wa.insert(new WeightedEdge<T>(nodes[3], nodes[5], 1)); // D->F
+
+		return wa;
+	}
 }
