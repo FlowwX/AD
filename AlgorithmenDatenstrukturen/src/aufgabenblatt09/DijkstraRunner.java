6b18ce2b34b51b695e406fbcc42b5e7add66fb9e
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-06-05 22:25:38 +0200

    little runner

---
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/DijkstraRunner.java
@@ -0,0 +1,52 @@
+package aufgabenblatt09;
+
+import java.util.Random;
+
+public class DijkstraRunner {
+	public static void main(String[] args) {
+		String fPath = "graph.bin";
+		DataCollector dc = new DataCollector();
+		dc.newLog("Matrix", "N", "counters");
+		dc.newLog("List", "N", "counters");
+		
+		for(int numNodes = 10; numNodes <= 1000; numNodes *= 10){
+			IWeightedGraph<Object> graph =WeightedGraphBuilder.getEmptyNodes(numNodes, AdjacencyImplementation.MATRIX);
+			graph.toFile(fPath);
+			
+			WeightedGraphMatrix<Object> graphMatrix = new WeightedGraphMatrix<Object>(0);
+			WeightedGraphList<Object> graphList = new WeightedGraphList<Object>();
+			
+			graphMatrix.fromFile(fPath);
+			graphList.fromFile(fPath);
+			
+			@SuppressWarnings("unchecked")
+			Node<Object> destination = (Node<Object>) graph.getNodes().toArray()[new Random().nextInt(numNodes)];
+			clearCounters(graphMatrix);
+			Dijkstra.calculate(graphMatrix, destination);
+			System.out.println("Finished Matrix for " + numNodes);
+			
+			clearCounters(graphList);
+			Dijkstra.calculate(graphList, destination);
+			System.out.println("Finished List for " + numNodes);
+			
+			dc.log("Matrix", numNodes, graphMatrix.countMapKeysGet+graphMatrix.countMatAccess+graphMatrix.countNodeIndMapGet);
+			dc.log("List", numNodes, graphList.countEdgeGet+graphList.countEdgeListGet+graphList.countNodeGet+graphList.countNodeListGet);
+		}
+		
+		dc.print("Matrix");
+		dc.print("List");
+	}
+	
+	public static void clearCounters(WeightedGraphMatrix<?> graph){
+		graph.countMapKeysGet = 0;
+		graph.countMatAccess = 0;
+		graph.countNodeIndMapGet = 0;
+	}
+	
+	public static void clearCounters(WeightedGraphList<?> graph){
+		graph.countEdgeGet = 0;
+		graph.countEdgeListGet = 0;
+		graph.countNodeGet = 0;
+		graph.countNodeListGet = 0;
+	}
+}
