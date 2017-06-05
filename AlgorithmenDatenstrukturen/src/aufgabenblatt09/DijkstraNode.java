e2f1702ec34536ecb1252a98575fee549d25005f
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-06-05 14:32:03 +0200

    Refactoring: DijkstraNode inherits Node (Issue #2)

--- AlgorithmenDatenstrukturen/src/aufgabenblatt09/DijkstraNode.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/DijkstraNode.java
@@ -1,13 +1,17 @@
 package aufgabenblatt09;
 
-public class DijkstraNode<T> {
-	public Node<T> node;
+public class DijkstraNode<T> extends Node<T>{
 	public DijkstraNode<T> next;
 	public int cost;
 
 	public DijkstraNode(Node<T> node, DijkstraNode<T> next, int cost) {
+		this(node.payload, next, cost);
+	}
+	
+	public DijkstraNode(T payload, DijkstraNode<T> next, int cost) {
+		super(payload);
+		
 		if(next == null) next = this;
-		this.node = node;
 		this.next = next;
 		this.cost = cost;
 	}
@@ -17,12 +21,13 @@ public class DijkstraNode<T> {
 		if(this == o) return true;
 		if(o == null) return false;
 		if(!(o instanceof DijkstraNode<?>)) return false;
+		@SuppressWarnings("unchecked")
 		DijkstraNode<T> on = (DijkstraNode<T>) o;
-		return this.node.equals(on.node);
+		return this.uid == on.uid;
 	}
 
 	@Override
 	public int hashCode(){
-		return this.node.uid;
+		return this.uid;
 	}
 }
