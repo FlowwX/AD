e116ae660a404df1cbbcea250197890dbc6a5809
Lasse Lüder <lasse.lueder@lueder-mail.de>
2017-06-05 16:21:41 +0200

    improvement using finished flag; comments; renamings

--- AlgorithmenDatenstrukturen/src/aufgabenblatt09/Dijkstra.java
+++ AlgorithmenDatenstrukturen/src/aufgabenblatt09/Dijkstra.java
@@ -1,66 +1,78 @@
 package aufgabenblatt09;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Dijkstra {
-	public static <T> List<DijkstraNode<T>> calculate(IWeightedGraph<T> graph, Node<T> destination) {
-		
-		ArrayList<DijkstraNode<T>> nodes = new ArrayList<DijkstraNode<T>>();
-		LinkedList<Node<T>> nodes_ = new LinkedList<Node<T>>();
-		HashSet<DijkstraNode<T>> randliste = new HashSet<DijkstraNode<T>>();
-		
-		DijkstraNode<T> cDest = new DijkstraNode<T>(destination, null, 0);
-		nodes.add(cDest);
-		nodes_.add(destination);
+	public static <T> Collection<DijkstraNode<T>> calculate(IWeightedGraph<T> graph, Node<T> destination) {
+		//// Initialize data structures
+		// set of known nodes to evaluate
+		HashSet<DijkstraNode<T>> boundary = new HashSet<DijkstraNode<T>>();
+		// mapping from Node (from to graph) to DijkstraNode with extra information
+		HashMap<Node<T>,DijkstraNode<T>> nodeToDijkstraNode = new HashMap<Node<T>,DijkstraNode<T>>();
+		for(Node<T> n : graph.getNodes()){
+			nodeToDijkstraNode.put(n, new DijkstraNode<>(n, null, Integer.MAX_VALUE-1)); // -1 wegen Vergleich unten
+		}
+
+		// the node that is the next step in order to get to the final destination
+		// start at destination node
+		DijkstraNode<T> nextNode = nodeToDijkstraNode.get(destination);
+		nextNode.cost = graph.getWeight(destination, destination);
 
 		do{
-			List<Node<T>> neighbours = graph.getNeighbours(cDest.node);
-			for(Node<T> randlistenKandidat : neighbours){
-				if(!nodes_.contains(randlistenKandidat)) {
-					int weight = graph.getWeight(randlistenKandidat, cDest.node);
-					randliste.add(new DijkstraNode<>(randlistenKandidat, cDest, cDest.cost + weight ));
+			nextNode.finished = true;
+
+			// add neighbours to boundary
+			List<Node<T>> neighbours = graph.getNeighbours(nextNode.node);
+			for(Node<T> boundaryCandidate : neighbours){
+				DijkstraNode<T> n = nodeToDijkstraNode.get(boundaryCandidate);
+				if(n.finished == false){
+					int weight = graph.getWeight(boundaryCandidate, nextNode.node);
+					n.cost = nextNode.cost + weight;
+					n.next = nextNode;
+					boundary.add(n);
 				}
 			}
-			
-			for(DijkstraNode<T> n : randliste){
-				if(graph.existsEdge(n.node, cDest.node)){
-					int weight = graph.getWeight(n.node, cDest.node);
-					if(n.cost > cDest.cost + weight){
-						n.cost = cDest.cost + weight;
-						n.next = cDest;
+
+			// for all considered nodes: if going over 'nextNode' is cheaper than the current way,
+			// set the way over "nextNode"
+			for(DijkstraNode<T> n : boundary){
+				if(graph.existsEdge(n.node, nextNode.node)){
+					int weight = graph.getWeight(n.node, nextNode.node);
+					if(n.cost > nextNode.cost + weight){
+						n.cost = nextNode.cost + weight;
+						n.next = nextNode;
 					}
 				}
 			}
-			
+
+			// remove node with minimum cost from boundary and set 'nextNode' to it
 			int minCost = Integer.MAX_VALUE;
 			DijkstraNode<T> minNode = null;
-			for(DijkstraNode<T> n : randliste){
-				if(graph.existsEdge(n.node, cDest.node)){
+			for(DijkstraNode<T> n : boundary){
+				if(graph.existsEdge(n.node, nextNode.node)){
 					if(n.cost < minCost){
 						minNode = n;
 						minCost = n.cost;
 					}
 				}
 			}
-			if(minNode == null) return nodes;
-			cDest = minNode;
-			nodes.add(cDest);
-			nodes_.add(cDest.node);
-			randliste.remove(cDest);
-		} while(randliste.size() > 0);
+			if(minNode == null) break; // boundary is empty, work done
+			nextNode = minNode;
+			boundary.remove(nextNode);
+		} while(boundary.size() > 0);
 
-		return nodes;
+		return nodeToDijkstraNode.values();
 	}
 
 	public static void main(String[] args) {
-		IWeightedGraph<Integer> wg = WeightedGraphFactory.getSpecialEmptyNodesAdjacency(10, AdjacencyImplementation.LIST);
+		IWeightedGraph<Integer> wg = WeightedGraphFactory.getSpecialEmptyNodesAdjacency(10, AdjacencyImplementation.MATRIX);
 		System.out.println(wg);
 
 		@SuppressWarnings("unchecked")
-		List<DijkstraNode<Integer>> dijkstraResult = calculate(wg, (Node<Integer>) WeightedGraphFactory.dest);
+		Collection<DijkstraNode<Integer>> dijkstraResult = calculate(wg, (Node<Integer>) WeightedGraphFactory.dest);
 		System.out.println("To node no. " + WeightedGraphFactory.dest.uid);
 		for (DijkstraNode<Integer> n : dijkstraResult) {
 			System.out.println("Node no. " + n.node.uid + ": costs " + n.cost + " over " + n.next.node.uid);
